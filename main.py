dc38569744eac47a30765848b92030be0de6271c
wishalta <rolandas.lebed@gmail.com>
2024-10-11 16:45:02 +0300

    9 done 10 50/50

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import secrets
 
 # 1
 
@@ -49,38 +50,71 @@ import random
 
 # 6
 
-txt = 'An American in Paris'
-a = "Breakfast at Tiffany's"
-b = "2001: A Space Odyssey"
-c = "It's a Wonderful Life"
-
-print(txt.translate({ord(i): None for i in 'aAeEoOuUIiy'}))      #kosmiska komanda translate, isgelbejo
-print(a.translate({ord(i): None for i in 'aAeEoOuUIiy'}))
-print(b.translate({ord(i): None for i in 'aAeEoOuUIiy'}))
-print(c.translate({ord(i): None for i in 'aAeEoOuUIiy'}))
-
-# 7
-
-starWars = "Star Wars: Episode " + (" " * random.randint(1, 9)) + str(random.randint(1, 7)) + " - A New Hope"
-
-print(starWars)
-
-print(starWars.translate({ord(i): None for i in 'AaĄąBbCcČčDdEeĘęĖėFfGgHhIiĮįYyJjKkLlMmNnOoPpRrSsŠšTtUuŲųŪūVvZzŽžwW- :'}))
-
-# 8
-
-# string = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
+# txt = 'An American in Paris'
+# a = "Breakfast at Tiffany's"
+# b = "2001: A Space Odyssey"
+# c = "It's a Wonderful Life"
 #
-# s = len(string.split())
-# print(s)
-
-
-
-# text = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
-# text = split()
-# def greater_5(text):
-# 	letters = [1 for word in text if len(word)>5]
+# print(txt.translate({ord(i): None for i in 'aAeEoOuUIiy'}))      #kosmiska komanda translate, isgelbejo
+# print(a.translate({ord(i): None for i in 'aAeEoOuUIiy'}))
+# print(b.translate({ord(i): None for i in 'aAeEoOuUIiy'}))
+# print(c.translate({ord(i): None for i in 'aAeEoOuUIiy'}))
 #
-# 	return sum(letters)
+# # 7
 #
-# print(greater_5(text))
+# starWars = "Star Wars: Episode " + (" " * random.randint(1, 9)) + str(random.randint(1, 7)) + " - A New Hope"
+#
+# print(starWars)
+#
+# print(starWars.translate({ord(i): None for i in 'AaĄąBbCcČčDdEeĘęĖėFfGgHhIiĮįYyJjKkLlMmNnOoPpRrSsŠšTtUuŲųŪūVvZzŽžwW- :'}))
+
+# 8 #10
+
+text = "Don't Be a Menace to South Central While Drinking Your Juice in the Hood"
+words = text.split()
+count = 0
+for word in words:
+    if len(word) <= 5:
+        count = count + 1
+
+print(count)
+
+txt = "Tik nereikia gąsdinti Pietų Centro, geriant sultis pas save kvartale"
+wordss = txt.split()
+count1 = 0
+for wordd in wordss:
+    if len(wordd) <= 5:
+        count1 = count1 + 1
+print(count1)
+
+def Convert(string):
+    li = list(string.split("f"))
+    return li
+print(Convert(text))
+print(Convert(txt))
+for  x in text:
+    print(random.choice(text))
+
+# listai, ciklai, salyginiai sakyniai
+# 9
+
+# from sys import stdout
+# from random import choice
+# from string import ascii_lowercase
+#
+# for _ in range(3):
+#     stdout.write(choice(ascii_lowercase))
+
+# 10
+
+
+#
+# def Convert(text):
+#     li = list(text.split(" "))
+#     return li
+#
+# gg = print(Convert(text,))
+#
+# gh = print(Convert(txt,))
+#
+# print(random.choice(gg))
