ea17e0531a687aa60097ec6b6a23ce19dd28c8f3
wishalta <rolandas.lebed@gmail.com>
2024-10-11 10:59:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# 1
+
+# pirmas = input('Jusu mylimo aktoriaus vardas: ')
+# # antras = input('Jusu mylimo aktoriaus pavarde: ')
+# #
+# # if len(pirmas) < len(antras):            # len skaiciuoja ilgi
+# #     print(f'{pirmas}')
+# # else:
+# #     print(f'{antras}')
+
+# 2
+
+pirmas = input('Jusu mylimo aktoriaus vardas: ')
+antras = input('Jusu mylimo aktoriaus pavarde: ')
+
+txt = pirmas.upper()                              # upper viskas ddideliom
+tzt = antras.lower()                              # lower viskas mazom
+
+print(f'{txt} {tzt}')
