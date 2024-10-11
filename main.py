aaa6f75d3718a11c70fccbdfc96984267c0b5fa7
wishalta <rolandas.lebed@gmail.com>
2024-10-11 11:24:24 +0300

    3 done

--- main.py
+++ main.py
@@ -11,10 +11,22 @@
 
 # 2
 
-pirmas = input('Jusu mylimo aktoriaus vardas: ')
-antras = input('Jusu mylimo aktoriaus pavarde: ')
+# pirmas = input('Jusu mylimo aktoriaus vardas: ')
+# antras = input('Jusu mylimo aktoriaus pavarde: ')
+#
+# txt = pirmas.upper()                              # upper viskas ddideliom
+# tzt = antras.lower()                              # lower viskas mazom
+#
+# print(f'{txt} {tzt}')
 
-txt = pirmas.upper()                              # upper viskas ddideliom
-tzt = antras.lower()                              # lower viskas mazom
+# 3
 
-print(f'{txt} {tzt}')
+# pirmas = input('Jusu mylimo aktoriaus vardas: ')
+# antras = input('Jusu mylimo aktoriaus pavarde: ')
+#
+# yeap = pirmas[:3]                                  # taip gali paimti pirmas raides is string
+# yeah = antras[:3]
+# print(f'{yeap}{yeah}')
+
+# 4
+
